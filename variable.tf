2a123aa25134a0ea713f53b4e2d273e66e3f66ae
akhilschandran4 <akhilschandran4@gmail.com>
2021-06-23 10:47:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "resource_name" {
+  type = string
+}
+
+variable "resource_location" {
+  type = string
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
