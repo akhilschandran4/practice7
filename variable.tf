17195f6486dcb3ed4c69b9e48312e6be7c1cc569
akhilschandran4 <akhilschandran4@gmail.com>
2021-06-23 11:45:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,15 @@
 variable "resource_name" {
   type = string
-  resource_name = "akhi"
 }
 
 variable "resource_location" {
   type = string
-  resource_location = "eastus"
 }
 
 variable "app_service_plan_name" {
   type = string
-  app_service_plan_name = "akhiappserviceplan"
 }
 
 variable "app_service_name" {
   type = string
-  app_service_name = "akhiappservice"
 }
