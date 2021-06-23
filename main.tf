fea550f60d85fe0f98307ec080ed08facf32d7ea
akhilschandran4 <akhilschandran4@gmail.com>
2021-06-23 11:43:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,23 @@
 terraform {
-  
- backend "azurerm" {
-  //   storage_account_name = "akhistorage"
-  //   container_name       = "akhicontainer"
-  //   key                  = "terraform.tfstate"
-	// access_key  = "imau4z20XBQuOoHQcAOlx++KKkqCX7khZNvi8hcjvSE3WEVqMlwqJi+2M8/R8TuSg5bQNC8WMmevy97/YeZf1A=="
-  features{}
+backend "azurerm" {
+    resource_group_name = "akhi-rg"
+    storage_account_name = "akhistorage"
+    container_name       = "akhicontainer"
 	}
 }
 provider "azurerm" {
-    version = "=2.13.0" 
-  # subscription_id             = "3457d5d0-1535-40c6-85fb-cb1919104698"
-  # client_id                   = "ac2f1e9d-1c78-4921-82ab-b145bb6bfc50"
-  # client_certificate_password = "0lY29KJJIAujfH56fkjy6o9C3_JfBubUFM"
-  # tenant_id                   = "afe00151-b24a-41de-97f7-be24c523f080"
-
- # az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/3457d5d0-1535-40c6-85fb-cb1919104698"
-
-   features {}
-}
-resource "azurerm_resource_group" "rsg" {
+  version = "~>2.46.0"
+    features {}
+  }
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_name
   location = var.resource_location
 }
 
 resource "azurerm_app_service_plan" "app_plan" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rsg.location
-  resource_group_name = azurerm_resource_group.rsg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
     tier = "Standard"
@@ -37,8 +27,8 @@ resource "azurerm_app_service_plan" "app_plan" {
 
 resource "azurerm_app_service" "web_app" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.rsg.location
-  resource_group_name = azurerm_resource_group.rsg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.app_plan.id
 
   site_config {
